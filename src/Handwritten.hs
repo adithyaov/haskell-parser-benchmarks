888fa5838edcd9ca114f873ffa4eb135aff92896
Finn Bender <mailobender77x@gmail.com>
2022-09-11 13:40:09 +0200

    Separate TokenTag and TokenValue

--- src/Handwritten.hs
+++ src/Handwritten.hs
@@ -1,42 +1,39 @@
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE StrictData #-}
 
-module Handwritten (parseFile, main, parse, expr, Pos (..)) where
+module Handwritten (parseFile, parse, expr, Pos (..)) where
 
-import Control.Exception (evaluate)
-import Control.Monad ((>=>))
 import Data.ByteString.Char8 qualified as BS
 import Data.Char (isDigit, isSpace, ord)
-import Data.Foldable (traverse_)
 import Data.Word (Word64)
-import System.Environment (getArgs)
 
 import Expr
-
-
-main :: IO ()
-main =
-    getArgs >>= traverse_ (parseFile >=> evaluate)
 
 
 parseFile :: FilePath -> IO (Maybe Expr)
 parseFile path = parse expr <$> BS.readFile path
 
 
-data Token
-    = TInt Word64
-    | TOp Op
-    | TParBeg
-    | TParEnd
-    | TEndOfFile
+data Token where
+    T ::
+        TokenTag a ->
+        a ->
+        {-# UNPACK #-} Pos ->
+        Token
+
+
+data TokenTag a where
+    TInt :: TokenTag Word64
+    TOp :: TokenTag Op
+    TEndOfFile :: TokenTag ()
 
 
 data LexerState
     = S
         Token
-        {-# UNPACK #-} Pos
         {-# UNPACK #-} BS.ByteString
 
 
@@ -44,41 +41,6 @@ data Pos = Pos
     { line :: {-# UNPACK #-} Word64
     , column :: {-# UNPACK #-} Word64
     }
-
-
-incLine :: Pos -> Pos
-incLine (Pos l c) = Pos (l + 1) c
-
-
-incColumnBy :: Int -> Pos -> Pos
-incColumnBy dc (Pos l c) = Pos l (c + fromIntegral dc)
-
-
-{-# INLINE nextTok #-}
-nextTok :: Pos -> BS.ByteString -> Maybe LexerState
-nextTok pos input =
-    let (ws, rest) = BS.span isSpace input
-        pos' = BS.foldl' updatePos pos ws
-     in case BS.uncons rest of
-            Nothing -> Just $ S TEndOfFile pos' BS.empty
-            Just (c, rest') -> case c of
-                '+' -> Just $ S (TOp Add) (incColumnBy 1 pos') rest'
-                '-' -> Just $ S (TOp Sub) (incColumnBy 1 pos') rest'
-                '*' -> Just $ S (TOp Mul) (incColumnBy 1 pos') rest'
-                '/' -> Just $ S (TOp Div) (incColumnBy 1 pos') rest'
-                '(' -> Just $ S (TParBeg) (incColumnBy 1 pos') rest'
-                ')' -> Just $ S (TParEnd) (incColumnBy 1 pos') rest'
-                _ | isDigit c -> lexNum pos' rest' $ digit c
-                _ -> Nothing
-    where
-        lexNum pos' input' n =
-            let (num_str, rest) = BS.span isDigit input'
-                num = BS.foldl' (\k c -> 10 * k + digit c) n num_str
-             in Just $ S (TInt num) (incColumnBy (1 + BS.length num_str) pos') rest
-
-        digit c = fromIntegral $ ord c - ord '0'
-        updatePos p '\n' = incLine p
-        updatePos p _ = incColumnBy (1 :: Int) p
 
 
 newtype Lexer a
@@ -91,6 +53,7 @@ newtype Lexer a
         )
 
 
+{-# INLINE unLexer #-}
 unLexer ::
     Lexer a ->
     forall r.
@@ -105,9 +68,9 @@ unLexer (Lexer f) = f
 
 
 runLexer :: Lexer a -> BS.ByteString -> Maybe a
-runLexer m bs = case nextTok (Pos 0 0) bs of
-    Nothing -> Nothing
-    Just (S tok pos inp) -> unLexer m (S tok pos inp) (const Nothing) (\_ a -> Just a)
+runLexer m input =
+    let initialState = S (T TEndOfFile () (Pos 0 0)) input
+     in unLexer (pop *> m) initialState (const Nothing) (const Just)
 
 
 parse :: Lexer a -> BS.ByteString -> Maybe a
@@ -117,6 +80,10 @@ parse = runLexer
 instance Monad Lexer where
     {-# INLINE (>>=) #-}
     m >>= f = Lexer $ \s r g -> unLexer m s r $ \s' a -> unLexer (f a) s' r g
+
+
+    {-# INLINE (>>) #-}
+    (>>) = (*>)
 
 
 instance Applicative Lexer where
@@ -147,17 +114,43 @@ empty = Lexer $ \s r _ -> r s
 
 {-# INLINE peek #-}
 peek :: Lexer Token
-peek = Lexer $ \s@(S t _ _) _ c -> c s t
+peek = Lexer $ \s@(S t _) _ c -> c s t
+
+
+updatePos :: Char -> Pos -> Pos
+updatePos '\n' (Pos l _) = Pos (l + 1) 0
+updatePos _ (Pos l c) = Pos l (c + 1)
+
+
+{-# INLINE popToken #-}
+popToken :: LexerState -> (LexerState -> r) -> (LexerState -> Token -> r) -> r
+popToken s@(S t@(T _ _ p) b) err ok = go p b
+    where
+        go pos bs = case BS.uncons bs of
+            Nothing -> ok (S (T TEndOfFile () pos) bs) t
+            Just (c, cs) ->
+                let pos' = updatePos c pos
+                 in case c of
+                        _ | isSpace c -> go pos' cs
+                        '+' -> ok (S (T TOp Add pos') cs) t
+                        '-' -> ok (S (T TOp Sub pos') cs) t
+                        '*' -> ok (S (T TOp Mul pos') cs) t
+                        '/' -> ok (S (T TOp Div pos') cs) t
+                        _ | isDigit c -> goNum pos' cs $ digit c
+                        _ -> err s
+        goNum pos bs n = case BS.uncons bs of
+            Nothing -> ok (S (T TInt n pos) bs) t
+            Just (c, cs)
+                | isDigit c -> goNum (updatePos c pos) cs (10 * n + digit c)
+                | otherwise -> ok (S (T TInt n pos) bs) t
+        digit c = fromIntegral $ ord c - ord '0'
 
 
 {-# INLINE pop #-}
 pop :: Lexer Token
-pop = Lexer $ \s@(S tok pos input) r c -> case nextTok pos input of
-    Nothing -> r s
-    Just s' -> c s' tok
+pop = Lexer popToken
 
 
-{-# INLINE expr #-}
 expr :: Lexer Expr
 expr = do
     a <- atom
@@ -170,21 +163,22 @@ expr = do
 atom :: Lexer Expr
 atom =
     pop >>= \case
-        TInt n -> pure $ Num n
+        T TInt n _ -> pure $ Num n
         _ -> empty
 
 
 sum' :: Expr -> Lexer Expr
 sum' = go
     where
+        go :: Expr -> Lexer Expr
         go !l =
             peek >>= \case
-                TOp Add -> do
+                T TOp Add _ -> do
                     _ <- pop
                     a <- atom
                     r <- prod a
                     go $ Bin Add l r
-                TOp Sub -> do
+                T TOp Sub _ -> do
                     _ <- pop
                     a <- atom
                     r <- prod a
@@ -195,13 +189,14 @@ sum' = go
 prod :: Expr -> Lexer Expr
 prod = go
     where
+        go :: Expr -> Lexer Expr
         go !l =
             peek >>= \case
-                TOp Mul -> do
+                T TOp Mul _ -> do
                     _ <- pop
                     r <- atom
                     go $ Bin Mul l r
-                TOp Div -> do
+                T TOp Div _ -> do
                     _ <- pop
                     r <- atom
                     go $ Bin Div l r
