9b82ca0e17551f76db3693fd8318289c48ede3a2
Finn Bender <mailobender77x@gmail.com>
2022-09-12 19:14:26 +0200

    Clean up gen-example.py and generate longer parenthesized expression

--- gen-example.py
+++ gen-example.py
@@ -10,15 +10,13 @@ with open("big-example.txt", "w+") as f:
         op = random.randrange(0, 4)
         f.write(ops[op])
         if random.randrange(0, 10) < 2:
-            if random.randrange(0, 1) == 0:
-                f.write('(')
-                depth += 1
+            f.write("(")
+            depth += 1
         n = random.randrange(0, 10000)
         f.write(f"{n}")
-        if random.randrange(0, 2) == 0 and depth > 0:
-            f.write(')')
+        if random.randrange(0, 9) < 2 and depth > 0:
+            f.write(")")
             depth -= 1
 
-
     for i in range(depth):
-        f.write(')')
+        f.write(")")
